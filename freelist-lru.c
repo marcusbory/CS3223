79fd246797534f843200be4a3bf48dbdb184ac37
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2023-02-17 10:49:39 +0800

    Change to int implementation

--- freelist-lru.c
+++ freelist-lru.c
@@ -228,20 +228,20 @@ StrategyUpdateAccessedBuffer(int buf_id, bool delete)
 		if (StrategyControl->head != -1) { // if head already exists
 			if (StrategyControl->tail == buf_id) { 	// if curr is tail
 				StrategyControl->tail = curr->prev; 	// point to prev buffer
-				&lruStack[curr->prev]->next = -1; 	// remove reference to curr pointer
+				&lruStack[curr->prev].next = -1; 	// remove reference to curr pointer
 			} else if (StrategyControl->head == buf_id) { // if curr is head
 				// do nothing
 			} else {
 				&lruStack[curr->prev].next = &lruStack[buf_id].next; 
 				&lruStack[curr->next].prev = &lruStack[buf_id].prev;
 			}
-			&lruStack[StrategyControl->head]->prev = buf_id; // set prev head to point at curr
+			&lruStack[StrategyControl->head].prev = buf_id; // set prev head to point at curr
 			&lruStack[buf_id].next = StrategyControl->head; // set curr to point at prev head
 		} else {
 			&lruStack[buf_id].next = -1;
 			StrategyControl->tail = buf_id;
 		} 
-		&lruStack[buf_id]->prev = -1;
+		&lruStack[buf_id].prev = -1;
 		StrategyControl->head = buf_id;
 	}
 }
@@ -396,7 +396,7 @@ StrategyGetBuffer(BufferAccessStrategy strategy, uint32 *buf_state)
 			return buf;
 		}
 		UnlockBufHdr(buf, local_buf_state);
-		curr_buf = &lruStack[curr_buf]->prev;
+		curr_buf = &lruStack[curr_buf].prev;
 	}
 
 	return NULL;
