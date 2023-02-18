4c0bb748ca66193bdf7ab7b33061e5ee09126830
ChewyChair <69298017+ChewyChair@users.noreply.github.com>
2023-02-18 21:51:40 +0800

    Remove usagecount check again

--- freelist-lru.c
+++ freelist-lru.c
@@ -308,7 +308,7 @@ StrategyGetBuffer(BufferAccessStrategy strategy, uint32 *buf_state)
 			 * of 8.3, but we'd better check anyway.)
 			 */
 			local_buf_state = LockBufHdr(buf);
-			if (BUF_STATE_GET_REFCOUNT(local_buf_state) == 0 && BUF_STATE_GET_USAGECOUNT(local_buf_state) == 0)
+			if (BUF_STATE_GET_REFCOUNT(local_buf_state) == 0)
 			{
 				if (strategy != NULL)
 					AddBufferToRing(strategy, buf);
